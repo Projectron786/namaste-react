3f331a6911935c8fcd0a519ad408a6aa7043e262
Abhishek Singh <abhishekthakur7841@gmail.com>
2024-01-07 16:21:37 +0530

    Episode 2 Igniting our app

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "this is a heading!"
-);
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-console.log(heading);
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", { id: "secondchild" }, "this is my heading1!🚀"),
+    React.createElement("h2", {}, "this is my heading2! 🚀"),
+  ]),
+]);
+
+console.log(parent);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(parent);
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-LEARNING REACT FROM AKSHAY SAINI
+# LEARNING REACT FROM AKSHAY SAINI
+
+# parcel
+
+- Dev Build
+- Local Server
+- HMR - Hot Module Replacement
+- File watchinf algorithm - written in c++ 💥
+- Caching - faster builds
+- Minification
+- Bundling
+- Compress
+- Consistent Hashing
+- Differential Bundling - to support older browsers
+- Diagnostic
+- Error handling
+- HTTPs
+- Tree shaking - remove unused code
