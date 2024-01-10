da014b6ae3978e7c668c574384235dea09b165ca
Abhishek30061998 <abhisheksingh.javrndcorp@gmail.com>
2024-01-10 21:10:33 +0530

    latest to fix main branch

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockdata";
 
 const Body = () => {
   const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.2599333&lng=77.412615&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+
+    console.log(json);
+  };
 
   return (
     <div className="body">
