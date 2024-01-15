4e037b5e81912dcfc9bf80cc36607ed5159e3d81
Abhishek30061998 <abhisheksingh.javrndcorp@gmail.com>
2024-01-16 04:01:39 +0530

    Episode-7 "Complete"

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,20 +1,21 @@
 import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
+import { MENU_URL } from "../utils/constants";
+import { useParams } from "react-router-dom";
 
 const RestaurantMenu = () => {
   const [resInfo, setResInfo] = useState(null);
+
+  const { resId } = useParams();
 
   useEffect(() => {
     fetchMenu();
   }, []);
 
   const fetchMenu = async () => {
-    const data = await fetch(
-      "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=23.2599333&lng=77.412615&restaurantId=520021&catalog_qa=undefined&submitAction=ENTER"
-    );
+    const data = await fetch(MENU_URL + resId);
 
     const json = await data.json();
-    console.log(json);
 
     setResInfo(json.data);
   };
@@ -24,11 +25,25 @@ const RestaurantMenu = () => {
   const { name, cuisines, costForTwoMessage } =
     resInfo?.cards[0]?.card?.card?.info;
 
+  const { itemCards } =
+    resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
+
   return (
     <div>
       <h2>{name}</h2>
-      <h2>{cuisines.join(", ")}</h2>
-      <h2>{costForTwoMessage}</h2>
+      <p>
+        {cuisines.join(", ")} - {costForTwoMessage}
+      </p>
+      <h2>Menu</h2>
+      <ul>
+        {itemCards.map((item) => (
+          <li key={item?.card?.info?.id}>
+            {item?.card?.info?.name} - {"Rs."}
+            {item?.card?.info?.price / 100 ||
+              item?.card?.info?.defaultPrice / 100}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
