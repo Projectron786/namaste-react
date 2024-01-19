898e5882c3dd1d8161d8e031b219c77dd7c3988d
Abhishek30061998 <abhisheksingh.javrndcorp@gmail.com>
2024-01-20 03:22:44 +0530

    Episode-08 | Let's Get Classy (Complete)

---
+++ src/components/User.js
@@ -0,0 +1,13 @@
+// import { useState } from "react";
+
+const User = ({ name }) => {
+  return (
+    <div className="user-card">
+      <h2>Name: {name}</h2>
+      <h3>Location: Bhopal</h3>
+      <h4>Contact: @abhishek</h4>
+    </div>
+  );
+};
+
+export default User;
